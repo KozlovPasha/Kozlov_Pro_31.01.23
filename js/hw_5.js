eba44bf954a377e508c98373123b80b442af997d
KozlovPasha <115036765+KozlovPasha@users.noreply.github.com>
2023-02-01 12:21:56 +0300

    add HW #5

---
+++ js/hw_5.js
@@ -0,0 +1,10 @@
+let firstNumber=Number (prompt ('Enter the first numbere!'));
+console.log(firstNumber);
+
+let secondNumber=Number (prompt ('Enter the second number!'));
+console.log(secondNumber);
+
+let thirdNumber= Number (prompt ('Enter the third number!'));
+console.log(thirdNumber);
+
+alert( (firstNumber + secondNumber+ thirdNumber) /3  + ' - The arithmetic mean of your numbers is !');
