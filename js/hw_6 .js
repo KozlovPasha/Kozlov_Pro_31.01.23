379643133a8f7a48f53f3236d47818b404f04a70
KozlovPasha <115036765+KozlovPasha@users.noreply.github.com>
2023-02-01 16:14:35 +0300

    add HW #6

---
+++ js/hw_6 .js
@@ -0,0 +1,21 @@
+//* HW #6   1/2
+const oneString = prompt ('enter a word #1')
+console.log(oneString)
+const twoString = prompt ('enter a word #2')
+console.log(twoString)
+const threeString = prompt ('enter a word #3')
+console.log(threeString)
+
+const allStrings = (oneString + twoString + threeString)
+console.log(allStrings)
+
+
+//* HW #6   2/2
+const numb = 12345;
+console.log(numb);
+
+const numbSrt = numb.toString()
+console.log(numbSrt);
+
+const numbWithSpace =(`${numbSrt [0]} ${numbSrt [1]} ${numbSrt [2]} ${numbSrt [3]} ${numbSrt [4]}`);
+console.log( numbWithSpace);
