0aba7e2e87716fcd30182d57cd5c0067a0a2af9b
KozlovPasha <115036765+KozlovPasha@users.noreply.github.com>
2023-02-06 12:14:41 +0300

    add HW # 12

---
+++ js/hw_12.js
@@ -0,0 +1,16 @@
+//* HW # 12
+const arrLenght = Number (prompt ('введите длинну масива'));
+const array = [];
+
+for (let index = 0; index < arrLenght; index++) {
+   const element = prompt (`введите номер элемента ${index}`);
+   array.push(element);
+}
+console.log(array)
+//сортування масиву за зростанням
+array.sort( (a, b) => a - b )
+console.log(array)
+
+//видалення елементів з (2 по 4 включно)
+array.splice(1,3)
+console.log(array)
