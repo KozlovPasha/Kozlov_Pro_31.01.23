5e92fe8fa5209f84948d0cddc0d988825f30359e
KozlovPasha <115036765+KozlovPasha@users.noreply.github.com>
2023-02-10 01:18:45 +0300

    add hw #15

---
+++ js/hw_15.js
@@ -0,0 +1,13 @@
+//* HW # 15
+const array =  ["apple", "carrot", "pear", "celery"]
+array.forEach(function(item) {
+    console.log(item)
+});
+
+
+let array1 = ["name", "surname", "family", "parent"]
+for (a of array1) {
+  console.log(a);
+}
+
+
