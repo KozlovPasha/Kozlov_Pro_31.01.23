44b1b7fe850e750b6f68a3c1e8b453979ad99739
KozlovPasha <115036765+KozlovPasha@users.noreply.github.com>
2023-04-05 22:35:20 +0300

    add HW #35

---
+++ Home Work #35/js/index.js
@@ -0,0 +1,15 @@
+//Вивести таблицю 10 × 10, заповнену числами від 1 до 100 (таблиця створюється динамічно)
+
+let table = document.querySelector('#table');
+
+for (let i = 0; i <= 9; i++) {
+	let tr = document.createElement('tr');
+	
+	for (let i = 0; i < 9; i++) {
+		let td = document.createElement('td');
+		td.innerText = Math.floor(Math.random() * 10) + 1;
+		tr.appendChild(td);
+	}
+	
+	table.appendChild(tr);
+}
